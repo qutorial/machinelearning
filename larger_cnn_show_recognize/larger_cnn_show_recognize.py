d79b4e0f00764d95ade5dc85268e27a4d7f7425b
Zaur <zaur@genua.de>
2018-01-01 20:23:05 +0100

    Large cnn and cnn

--- larger_cnn_show_recognize/larger_cnn_show_recognize.py
+++ larger_cnn_show_recognize/larger_cnn_show_recognize.py
@@ -7,7 +7,17 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.utils import np_utils
+
+# loading and saving models
 from keras.models import load_model
+
+
+# cnn stuff
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras import backend as K
+K.set_image_dim_ordering('th')
 
 import argparse
 parser = argparse.ArgumentParser(description='Recognizing hand written mnist numbers.')
@@ -22,10 +32,9 @@ numpy.random.seed(seed)
 # load data
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-# flatten 28*28 images to a 784 vector for each image
-num_pixels = X_train.shape[1] * X_train.shape[2]
-X_train = X_train.reshape(X_train.shape[0], num_pixels).astype('float32')
-X_test = X_test.reshape(X_test.shape[0], num_pixels).astype('float32')
+# reshape to be [samples][pixels][width][height]
+X_train = X_train.reshape(X_train.shape[0], 1, 28, 28).astype('float32')
+X_test = X_test.reshape(X_test.shape[0], 1, 28, 28).astype('float32')
 
 # normalize inputs from 0-255 to 0-1
 X_train = X_train / 255
@@ -36,12 +45,20 @@ y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
 num_classes = y_test.shape[1]
 
-# define baseline model
-def baseline_model():
+
+# define the larger model
+def larger_model():
   # create model
   model = Sequential()
-  model.add(Dense(num_pixels, input_dim=num_pixels, kernel_initializer='normal', activation='relu'))
-  model.add(Dense(num_classes, kernel_initializer='normal', activation='softmax'))
+  model.add(Conv2D(30, (5, 5), input_shape=(1, 28, 28), activation='relu'))
+  model.add(MaxPooling2D(pool_size=(2, 2)))
+  model.add(Conv2D(15, (3, 3), activation='relu'))
+  model.add(MaxPooling2D(pool_size=(2, 2)))
+  model.add(Dropout(0.2))
+  model.add(Flatten())
+  model.add(Dense(128, activation='relu'))
+  model.add(Dense(50, activation='relu'))
+  model.add(Dense(num_classes, activation='softmax'))
   # Compile model
   model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
   return model
@@ -49,9 +66,9 @@ def baseline_model():
 
 def train_model():
   # build the model
-  model = baseline_model()
+  model = larger_model()
   # Fit the model
-  model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200, verbose=2)
+  model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=15, batch_size=200, verbose=2)
   # Final evaluation of the model
   scores = model.evaluate(X_test, y_test, verbose=0)
   print("Baseline Error: %.2f%%" % (100-scores[1]*100))
@@ -71,10 +88,9 @@ except:
 
 
 
-# some index in test samples to recognize and show
 print("Recongnizing number in the sample #%d" % n)
-
 print("It is %d" % model.predict_classes(X_test[n:n+1])[0] )
+
 
 import matplotlib.pyplot as plt
 plt.subplot(111)
